c3ea964466a7949b4ca52f9ce51e7fab1d6cb103
Vyacheslav Pugovkin <72509800+ideahold@users.noreply.github.com>
2021-11-08 23:34:02 +0300

    Update asyncio1.py

--- asyncio1.py
+++ asyncio1.py
@@ -1,26 +1,26 @@
-import asyncio
+import asyncio 
 
-async def f():
+async def f(): # first coroutine
 	
 	print("Running f")
 	await asyncio.sleep(0)
 	print("We're here again")
 
 
-async def pb():
+async def pb(): # second coroutine
 
 	print("Content to pb")
 	await asyncio.sleep(0)
 	print("Back to pb")
 
 
-async def main():
+async def main(): # 
 
 	tasks = [
 		f(),
 		pb()
-	]
+	] # our tasks
 
-	await asyncio.gather(*tasks)
+	await asyncio.gather(*tasks) # if smth from tasks is coroutine it becomes a task 
 
 asyncio.run(main())
