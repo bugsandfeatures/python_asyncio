f58ef69e6b41de5ac9509aa0e07a13eea62cfef2
Vyacheslav Pugovkin <72509800+ideahold@users.noreply.github.com>
2021-11-08 23:38:02 +0300

    Add files via upload

---
+++ asyncio2.py
@@ -0,0 +1,49 @@
+from time import time
+import asyncio
+
+start = time()
+
+def tic():
+
+	return 'at %1.1f sec' % (time() - start)
+
+
+async def f1():
+
+	print(f'f1 started {tic()}')
+	await asyncio.sleep(2)
+	print(f'f1 ended {tic()}')
+
+
+async def f2():
+
+	print(f'f2 started {tic()}')
+	await asyncio.sleep(2)
+	print(f'f2 ended {tic()}')
+
+
+async def f3():
+
+	print(f'while other coroutines are blocked {tic()}')
+	await asyncio.sleep(1)
+	print('Done!')
+
+
+async def main():
+
+	tasks = [
+		f1(), 
+		f2(), 
+		f3()
+	]
+
+	await asyncio.gather(*tasks)
+
+
+asyncio.run(main())
+
+
+
+
+
+
